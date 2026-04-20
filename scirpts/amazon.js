eb16ae86dd93ba1722f7bd72a2b01915bb9ead1a
Endiris <edristaju@gmail.com>
2026-04-20 15:13:17 +0300

    Make cart quantity ineractive

--- scirpts/amazon.js
+++ scirpts/amazon.js
@@ -73,8 +73,11 @@ button.addEventListener('click',()=>{
     quantity:1
   });
   }
-
-  console.log(cart);
-  
-})
-})
+ let cartQuantity=0;
+ cart.forEach((item)=>{
+  cartQuantity+=item.quantity
+ });
+ document.querySelector('.js-cart-quantity').innerHTML=cartQuantity; 
+});
+});
+ 
