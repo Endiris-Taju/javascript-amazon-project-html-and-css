7900578f643d3f83792844ff6460dd5f54794d20
Endiris <edristaju@gmail.com>
2026-04-21 21:25:54 +0300

    save cart in localstorage

--- data/cart.js
+++ data/cart.js
@@ -1,4 +1,7 @@
-export let cart=[{
+export let cart= JSON.parse(localStorage.getItem ('cart'));
+
+if(!cart){
+  cart =[{
   productId:'e43638ce-6aa0-4b85-b27f-e1d07eb678c6',
   quantity:2
 },
@@ -6,13 +9,20 @@ export let cart=[{
   productId:'15b6fc6f-327a-4ec4-896f-486349e85a3d',
   quantity:1
 }];
+}
 
+
+
+ function saveToStorage(){
+  localStorage.setItem('cart', JSON.stringify(cart));
+ };
  export function addToCart(productId){
     let matchingItem;
       cart.forEach((cartItem)=>{
         if(productId === cartItem.productId){
           matchingItem = cartItem;
         }
+        saveToStorage();
       });
 
       const quantitySelector= document.querySelector(`.js-quantity-selector-${productId}`);
@@ -38,4 +48,5 @@ export let cart=[{
      }
     });
     cart = newCart;
+    saveToStorage();
    };
