c9802a5e22e6cf258593857a5cd47ecd1ac200b2
Endiris <edristaju@gmail.com>
2026-04-21 17:42:22 +0300

    Delete products from cart when clicking delete

--- data/cart.js
+++ data/cart.js
@@ -1,4 +1,4 @@
-export const cart=[{
+export let cart=[{
   productId:'e43638ce-6aa0-4b85-b27f-e1d07eb678c6',
   quantity:2
 },
@@ -23,11 +23,19 @@ export const cart=[{
         matchingItem.quantity +=quantity;
       }else{
         cart.push({
-        //productId:productId,
-        //quantity:quantity
         //shortcuts
         productId,
         quantity
       });
     }
-   }
+   };
+
+   export function removeFromCart(productId){
+    const newCart= [];
+    cart.forEach((cartItem)=>{
+     if(cartItem.productId !== productId){
+      newCart.push(cartItem);
+     }
+    });
+    cart = newCart;
+   };
