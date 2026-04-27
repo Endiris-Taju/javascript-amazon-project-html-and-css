9693f605ea8171788d7859668c2a7190fe281cb2
Endiris <edristaju@gmail.com>
2026-04-27 14:46:36 +0300

    Remove defaul tests and add money tests

---
+++ test-jasmine/moneyTest.js
@@ -0,0 +1,12 @@
+import { formatCurrency } from "../scirpts/utils/money.js";
+describe('test suit: formatCurrency', ()=>{
+  it('convert dollars to cents',()=>{
+    expect(formatCurrency(2095)).toEqual('20.95')
+  });
+   it('convert dollars to cents',()=>{
+    expect(formatCurrency(0)).toEqual('0.00')
+  });
+   it('round to next cents',()=>{
+    expect(formatCurrency(2000.5)).toEqual('20.01')
+  });
+})
